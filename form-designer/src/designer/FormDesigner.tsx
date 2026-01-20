b263442644db1ae38b52dde995f042b82d616f95
yy3 <yuyue0534@gmail.com>
2026-01-20 11:04:43 +0800

    Add files via upload

--- form-designer/src/designer/FormDesigner.tsx
+++ form-designer/src/designer/FormDesigner.tsx
@@ -9,21 +9,21 @@ export function FormDesigner() {
     const { state } = useDesigner();
 
     return (
-        <div className="h-screen w-screen overflow-hidden bg-white">
+        <div className="h-[100dvh] w-screen overflow-hidden bg-white flex flex-col">
             <TopBar />
 
-            <div className="grid h-[calc(100vh-56px)] grid-cols-12">
+            <div className="grid flex-1 min-h-0 grid-cols-12">
                 {/* Left */}
-                <div className="col-span-3 min-w-[260px]">
+                <div className="col-span-3 min-h-0 min-w-[260px] overflow-hidden">
                     <ComponentPalette />
                 </div>
 
                 {/* Center */}
-                <div className="col-span-6">
+                <div className="col-span-6 min-h-0 overflow-hidden">
                     {state.mode === "design" ? (
                         <Canvas />
                     ) : (
-                        <div className="h-full overflow-auto bg-slate-50 p-6">
+                        <div className="h-full min-h-0 overflow-y-auto bg-slate-50 p-6">
                             <div className="mx-auto max-w-3xl rounded-2xl border border-slate-200 bg-white p-6">
                                 <FormRenderer schema={state.schema} />
                             </div>
@@ -32,7 +32,7 @@ export function FormDesigner() {
                 </div>
 
                 {/* Right */}
-                <div className="col-span-3 min-w-[280px]">
+                <div className="col-span-3 min-h-0 min-w-[280px] overflow-hidden">
                     <PropertyEditor />
                 </div>
             </div>
